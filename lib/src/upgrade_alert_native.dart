6045801f449de9ff24ec0807844d93748a44dfdc
najm101 <bodinegem@icloud.com>
2025-05-17 15:20:00 +0300

    feat: Migrate Android project to declarative plugin DSL and update Gradle configurations
    - Migrated `build.gradle` files to use the new declarative plugin DSL.
    - Updated Android SDK versions and configurations.
    - Added new files for CMake build outputs in the Android project.
    - Introduced `UpgradeAlertNative` widget for native upgrade alerts.
    - Integrated `native_dialog_plus` package for dialog handling.
    - Updated iOS project settings, including bundle identifiers and team IDs.
    - Added support for GPU validation mode in Xcode schemes.
    - Updated `pubspec.yaml` to include `native_dialog_plus` dependency.

---
+++ lib/src/upgrade_alert_native.dart
@@ -0,0 +1,242 @@
+/*
+ * Copyright (c) 2021-2024 Larry Aasen. All rights reserved.
+ */
+import 'package:native_dialog_plus/native_dialog_plus.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'upgrade_messages.dart';
+import 'upgrade_state.dart';
+import 'upgrader.dart';
+
+/// A widget to display the upgrade dialog.
+/// Override the [createState] method to provide a custom class
+/// with overridden methods.
+class UpgradeAlertNative extends StatefulWidget {
+  /// Creates a new [UpgradeAlertNative].
+  UpgradeAlertNative({
+    super.key,
+    Upgrader? upgrader,
+    this.onIgnore,
+    this.onLater,
+    this.onUpdate,
+    this.shouldPopScope,
+    this.showIgnore = true,
+    this.showLater = true,
+    this.showReleaseNotes = true,
+    this.child,
+  }) : upgrader = upgrader ?? Upgrader.sharedInstance;
+
+  /// The upgraders used to configure the upgrade dialog.
+  final Upgrader upgrader;
+
+  /// Called when the ignore button is tapped or otherwise activated.
+  /// Return false when the default behavior should not execute.
+  final BoolCallback? onIgnore;
+
+  /// Called when the later button is tapped or otherwise activated.
+  final BoolCallback? onLater;
+
+  /// Called when the update button is tapped or otherwise activated.
+  /// Return false when the default behavior should not execute.
+  final BoolCallback? onUpdate;
+
+  /// Called to determine if the dialog blocks the current route from being popped.
+  final BoolCallback? shouldPopScope;
+
+  /// Hide or show Ignore button on dialog (default: true)
+  final bool showIgnore;
+
+  /// Hide or show Later button on dialog (default: true)
+  final bool showLater;
+
+  /// Hide or show release notes (default: true)
+  final bool showReleaseNotes;
+
+  /// The [child] contained by the widget.
+  final Widget? child;
+
+  @override
+  UpgradeAlertNativeState createState() => UpgradeAlertNativeState();
+}
+
+/// The [UpgradeAlertNative] widget state.
+class UpgradeAlertNativeState extends State<UpgradeAlertNative> {
+  /// Is the alert dialog being displayed right now?
+  bool displayed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.upgrader.initialize();
+  }
+
+  /// Describes the part of the user interface represented by this widget.
+  @override
+  Widget build(BuildContext context) {
+    if (widget.upgrader.state.debugLogging) {
+      print('upgrader: build UpgradeAlertNative');
+    }
+
+    return StreamBuilder(
+      initialData: widget.upgrader.state,
+      stream: widget.upgrader.stateStream,
+      builder: (BuildContext context, AsyncSnapshot<UpgraderState> snapshot) {
+        if ((snapshot.connectionState == ConnectionState.waiting ||
+                snapshot.connectionState == ConnectionState.active) &&
+            snapshot.data != null) {
+          final upgraderState = snapshot.data!;
+          if (upgraderState.versionInfo != null) {
+            if (widget.upgrader.state.debugLogging) {
+              print("upgrader: need to evaluate version");
+            }
+
+            if (!displayed) {
+              checkVersion();
+            }
+          }
+        }
+        return widget.child ?? const SizedBox.shrink();
+      },
+    );
+  }
+
+  /// Will show the alert dialog when it should be dispalyed.
+  void checkVersion() {
+    final shouldDisplay = widget.upgrader.shouldDisplayUpgrade();
+    if (widget.upgrader.state.debugLogging) {
+      print('upgrader: shouldDisplayReleaseNotes: $shouldDisplayReleaseNotes');
+    }
+    if (shouldDisplay) {
+      displayed = true;
+      final appMessages = widget.upgrader.determineMessages(context);
+
+      Future.delayed(Duration.zero, () {
+        showTheDialog(
+          title: appMessages.message(UpgraderMessage.title),
+          message: widget.upgrader.body(appMessages),
+          releaseNotes:
+              shouldDisplayReleaseNotes ? widget.upgrader.releaseNotes : null,
+          messages: appMessages,
+        );
+      });
+    }
+  }
+
+  void onUserIgnored(BuildContext context, bool shouldPop) {
+    if (widget.upgrader.state.debugLogging) {
+      print('upgrader: button tapped: ignore');
+    }
+
+    // If this callback has been provided, call it.
+    final doProcess = widget.onIgnore?.call() ?? true;
+
+    if (doProcess) {
+      widget.upgrader.saveIgnored();
+    }
+
+    if (shouldPop) {
+      popNavigator(context);
+    }
+  }
+
+  void onUserLater(BuildContext context, bool shouldPop) {
+    if (widget.upgrader.state.debugLogging) {
+      print('upgrader: button tapped: later');
+    }
+
+    // If this callback has been provided, call it.
+    widget.onLater?.call();
+
+    if (shouldPop) {
+      popNavigator(context);
+    }
+  }
+
+  void onUserUpdated(BuildContext context, bool shouldPop) {
+    if (widget.upgrader.state.debugLogging) {
+      print('upgrader: button tapped: update now');
+    }
+
+    // If this callback has been provided, call it.
+    final doProcess = widget.onUpdate?.call() ?? true;
+
+    if (doProcess) {
+      widget.upgrader.sendUserToAppStore();
+    }
+
+    if (shouldPop) {
+      popNavigator(context);
+    }
+  }
+
+  void popNavigator(BuildContext context) {
+    displayed = false;
+  }
+
+  bool get shouldDisplayReleaseNotes =>
+      widget.showReleaseNotes &&
+      (widget.upgrader.releaseNotes?.isNotEmpty ?? false);
+
+  /// Show the alert dialog.
+  void showTheDialog({
+    required String? title,
+    required String message,
+    required String? releaseNotes,
+    required UpgraderMessages messages,
+  }) {
+    if (widget.upgrader.state.debugLogging) {
+      print('upgrader: showTheDialog title: $title');
+      print('upgrader: showTheDialog message: $message');
+      print('upgrader: showTheDialog releaseNotes: $releaseNotes');
+    }
+
+    // Save the date/time as the last time alerted.
+    widget.upgrader.saveLastAlerted();
+
+    final isBlocked = widget.upgrader.blocked();
+    final showIgnore = isBlocked ? false : widget.showIgnore;
+    final showLater = isBlocked ? false : widget.showLater;
+
+    NativeDialogPlus(
+      actions: [
+        NativeDialogPlusAction(
+          text: messages.message(UpgraderMessage.buttonTitleUpdate) ?? '',
+          onPressed: () => onUserUpdated(context, !widget.upgrader.blocked()),
+        ),
+        if (showLater)
+          NativeDialogPlusAction(
+            text: messages.message(UpgraderMessage.buttonTitleLater) ?? '',
+            onPressed: () => onUserLater(context, true),
+            style: NativeDialogPlusActionStyle.cancel,
+          ),
+        if (showIgnore)
+          NativeDialogPlusAction(
+            text: messages.message(UpgraderMessage.buttonTitleIgnore) ?? '',
+            onPressed: () => onUserIgnored(context, true),
+            style: NativeDialogPlusActionStyle.destructive,
+          ),
+      ],
+      title: title ?? '',
+      message:
+          "$message ${shouldDisplayReleaseNotes ? "\n\n $releaseNotes" : ''}",
+    ).show();
+  }
+
+  /// Determines if the dialog blocks the current route from being popped.
+  /// Will return the result from [shouldPopScope] if it is not null, otherwise it will return false.
+  bool onCanPop() {
+    if (widget.upgrader.state.debugLogging) {
+      print('upgrader: onCanPop called');
+    }
+    if (widget.shouldPopScope != null) {
+      final should = widget.shouldPopScope!();
+      if (widget.upgrader.state.debugLogging) {
+        print('upgrader: shouldPopScope=$should');
+      }
+      return should;
+    }
+
+    return false;
+  }
+}
